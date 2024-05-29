f5dafab4d9c66fc4f58db9251a73fc9421862b94
Daniella Akpoguma <rukevweakpoguma11@gmail.com>
2024-05-29 19:12:34 -0400

    Changed project structure

---
+++ Python Projects/Web Scraper/Homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multipage App"
+)
+
+
+st.title("Home Page")
+st.sidebar.success("Select a page above")
--- Python Projects/Web Scraper/home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show_home():
-    st.title("Dashboard")
-    st.write("Welcome to the Web Scraper App!")
