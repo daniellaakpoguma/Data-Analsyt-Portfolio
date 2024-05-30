ffa133f4bddf9f0c538455f31e8cfe71426fce73
Daniella Akpoguma <rukevweakpoguma11@gmail.com>
2024-05-29 21:47:56 -0400

    Added new data set and code for web scraper

--- Python Projects/Web Scraper & Dashboard/1_🏡_Homepage.py
+++ Python Projects/Web Scraper & Dashboard/1_🏡_Homepage.py
@@ -25,8 +25,3 @@ This dataset contains information related to buyer reviews and evaluations of pr
 The data includes various attributes that provide insights into the sentiment, feedback, and interaction surrounding product evaluations.
 """)
 
-if "dataset" not in st.session_state:
-    st.session_state.dataset = pd.read_csv("C:\\Users\\rukev\\OneDrive\\Desktop\\Data Analsyt Portfolio\\Data-Analsyt-Portfolio\\Data Sets\\Online Retail Business\\OnlineRetail.csv", encoding='latin1')
-
-st.subheader("Dataset Preview")
-st.write(st.session_state.dataset)
--- Python Projects/Web Scraper & Dashboard/pages/2_👩‍💻_Dashboard.py
+++ Python Projects/Web Scraper & Dashboard/pages/2_👩‍💻_Dashboard.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
 
 st.title("Dashboard")
+
+# Load Data
+df = pd.read_csc("C:\\Users\\rukev\\OneDrive\\Desktop\\Data Analsyt Portfolio\\Data-Analsyt-Portfolio\\Data Sets\\Food and Services Drinking PLaces\\21100232.csv")
+
+
+
