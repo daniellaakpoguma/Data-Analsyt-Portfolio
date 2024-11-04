1edc4c63954c539df2373ebf9d084fe34bd56f12
Daniella Akpoguma <rukevweakpoguma11@gmail.com>
2024-11-04 12:00:18 -0500

    Added background image

--- Microsoft Excel/Bakery Sales Dashboard/Market Basket Analysis.R
+++ Microsoft Excel/Bakery Sales Dashboard/Market Basket Analysis.R
@@ -18,9 +18,29 @@ print(bakery_sales)
 bakery_sales_basket <- subset(bakery_sales, select = -c(datetime, time, `day of week`, `total sales`))
 print(bakery_sales_basket)
 
-# Item Frequency
-# x <- bakery_sales_basket
-# y <-
-# barplot()
+# Convert to binary
+bakery_sales_basket_binary <- as.data.frame(lapply(bakery_sales_basket, function(x) ifelse(!is.na(x) & x > 0, 1, 0)))
+print(bakery_sales_basket_binary)
+
+# Convert to transactions
+transactions <- as(as.matrix(bakery_sales_basket_binary), "transactions")
+print(transactions)
+
+itemFrequencyPlot(transactions, topN = 10, type = "absolute")
+
+# Apply the apriori algorithm
+rules <- apriori(transactions, parameter = list(supp = 0.01, conf = 0.8))
+inspect(rules)
+
+# # Graph plot
+# plot(rules, method = "graph", control = list(type = "items"))
+# 
+# # Grouped matrix plot
+# plot(rules, method = "grouped")
+# 
+# # Matrix plot
+# plot(rules, method = "matrix", control = list(reorder = TRUE))
+
+
 
 
