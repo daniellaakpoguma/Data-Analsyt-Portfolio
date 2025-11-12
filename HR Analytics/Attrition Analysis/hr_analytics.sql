e9aeee1dca5db9141ec3257cf64686712e7f496b
kewiz2 <rukevweakpoguma11@gmail.com>
2025-11-12 17:16:06 -0500

    Added changes

--- HR Analytics/Attrition Analysis/hr_analytics.sql
+++ HR Analytics/Attrition Analysis/hr_analytics.sql
@@ -37,8 +37,29 @@ SELECT Gender, COUNT(*) AS no_of_employees
 FROM hr_employee_attrition
 GROUP BY Gender;
 # 4. Average years at company
+SELECT AVG(YearsAtCompany)  AS average_years_at_working
+FROM hr_employee_attrition;
+#5. Average monthly income
+SELECT AVG(MonthlyIncome) AS avg_monthly_income
+FROM  hr_employee_attrition;
+#6. Most common job role
+SELECT JobRole, COUNT(*)
+FROM  hr_employee_attrition
+GROUP BY JobRole;
+#7. Most common education field
+SELECT EducationField, COUNT(*) AS no_of_employees
+FROM hr_employee_attrition
+GROUP BY EducationField;
 
+## ATTRITION INSIGHTS
+## Personal Information
+### Attrition by Gender
+SELECT Gender, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition, Gender
+ORDER BY Gender, Attrition ASC;
 
+### Attrition by Age Group
 # Create view for Age Group Summary 
 CREATE VIEW age_group_summary AS
 SELECT CASE
@@ -48,65 +69,91 @@ SELECT CASE
     ELSE "41-60"
 END AS AgeGroup, Age, Department, EducationField, Gender, MaritalStatus, Attrition
 FROM hr_employee_attrition;
-
 # Testing of view
 SELECT * FROM age_group_summary;
-
 # No of employees per age group
 SELECT AgeGroup, COUNT(*) AS no_of_employees
 FROM age_group_summary
 GROUP BY AgeGroup;
-
-# No of employees per education field
-SELECT EducationField, COUNT(*) AS no_of_employees
-FROM hr_employee_attrition
-GROUP BY EducationField;
-
-## Attrition Insights
-CREATE VIEW total_employees AS
-SELECT COUNT(*) AS total_employees
-FROM hr_employee_attrition;
-
-SELECT *
-FROM total_employees;
-
-# General Attrition Stats
-# Yes = Employees that have left the company
-# No = Employees that are still at the company
-SELECT Attrition, COUNT(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
-FROM hr_employee_attrition
-GROUP BY Attrition;
-
-# Attrition by Department
-SELECT Department, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
-FROM hr_employee_attrition
-GROUP BY Attrition, Department
-ORDER BY Department, Attrition ASC;
-
-# Attrition by Gender
-SELECT Gender, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
-FROM hr_employee_attrition
-GROUP BY Attrition, Gender
-ORDER BY Gender, Attrition ASC;
-
 # Attrition by Age Group
 SELECT AgeGroup, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
 FROM age_group_summary
 GROUP BY Attrition, AgeGroup
 ORDER BY AgeGroup, Attrition ASC;
-# Age Group (18-25) has a signifactly higher rate of attrition
 
-# Attrition by Job Level
+### Attrition by Marital Status
+SELECT MaritalStatus, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition, MaritalStatus
+ORDER BY MaritalStatus, Attrition ASC;
+
+### Attrition by Educational Field
+SELECT EducationField, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition, EducationField
+ORDER BY EducationField, Attrition ASC;
+
+## Job Information
+### Attrition by Department
+SELECT Department, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition, Department
+ORDER BY Department, Attrition ASC;
+
+### Attrition by Job Level
 SELECT JobLevel, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
 FROM hr_employee_attrition
 GROUP BY Attrition, JobLevel
 ORDER BY JobLevel, Attrition ASC;
 
-# Attrition by Job Involvement
+### Attrition by Job Involvement
 SELECT JobInvolvement, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
 FROM hr_employee_attrition
 GROUP BY Attrition, JobInvolvement
 ORDER BY JobInvolvement, Attrition ASC;
+
+### Attrition by Job Satisfaction
+SELECT JobSatisfaction, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition, JobSatisfaction
+ORDER BY JobSatisfaction, Attrition ASC;
+
+# Attrition by MonthlyIncome
+SELECT  MonthlyIncome, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition,  MonthlyIncome
+ORDER BY  MonthlyIncome, Attrition ASC;
+
+# Attrition By Income Group
+CREATE OR REPLACE VIEW income_groups AS
+SELECT
+    EmployeeNumber,
+    MonthlyIncome,
+    Attrition,
+    CASE
+        WHEN MonthlyIncome BETWEEN 1000 AND 4999 THEN 'Low'
+        WHEN MonthlyIncome BETWEEN 5000 AND 9999 THEN 'Medium'
+        WHEN MonthlyIncome BETWEEN 10000 AND 14999 THEN 'High'
+        WHEN MonthlyIncome BETWEEN 15000 AND 20000 THEN 'Very High'
+        ELSE 'Other'
+    END AS income_group
+FROM hr_employee_attrition;
+
+SELECT
+    income_group,
+    COUNT(*) AS employee_count,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,
+    ROUND(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS attrition_percentage
+FROM income_groups
+GROUP BY income_group
+ORDER BY attrition_percentage DESC;
+
+# Attrition by StockOptionLevel
+SELECT StockOptionLevel, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
+FROM hr_employee_attrition
+GROUP BY Attrition, StockOptionLevel
+ORDER BY StockOptionLevel, Attrition ASC;
+
 
 DROP VIEW distancefromhome_summary;
 
@@ -129,12 +176,6 @@ ORDER BY distance_group;
 # Attrition by Distance Frome Home
 SELECT distance_group, Attrition, employee_count, ROUND(employee_count * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
 FROM distancefromhome_summary;
-
-# Attrition by Job  Satisfaction
-SELECT JobSatisfaction, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
-FROM hr_employee_attrition
-GROUP BY Attrition, JobSatisfaction
-ORDER BY JobSatisfaction, Attrition ASC;
 
 # Attrition by Performance Rating
 SELECT PerformanceRating, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
@@ -165,9 +206,3 @@ SELECT WorkLifeBalance, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 
 FROM hr_employee_attrition
 GROUP BY Attrition, WorkLifeBalance
 ORDER BY WorkLifeBalance, Attrition ASC;
-
-# Attrition by StockOptionLevel
-SELECT StockOptionLevel, Attrition, Count(*) AS employee_count, ROUND(COUNT(*) * 100.0 / (SELECT total_employees FROM total_employees), 2) AS percentage
-FROM hr_employee_attrition
-GROUP BY Attrition, StockOptionLevel
-ORDER BY StockOptionLevel, Attrition ASC;
